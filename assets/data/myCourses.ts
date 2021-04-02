6f36819f5111e86a4af07cb39417fd07b6055e65
dhill219 <65919087+dhill219@users.noreply.github.com>
2021-04-02 17:59:05 -0400

    Add files via upload

---
+++ assets/data/myCourses.ts
@@ -0,0 +1,7 @@
+import {courses} from '../../app/courses';
+export const COURSES : courses[]=[
+{ course_code: 121134549,course_title:"Mobile web based Application",dayofweek:"friday",professor_name:"Andy pak"},
+{ course_code: 12145678,course_title:"web programming",dayofweek:"tuesday",professor_name:"ronak"},
+{ course_code: 14675449,course_title:"java",dayofweek:"wednesday",professor_name:"wael channa"},
+{ course_code: 12442549,course_title:"c#",dayofweek:"friday",professor_name:"abdullah"}
+];
